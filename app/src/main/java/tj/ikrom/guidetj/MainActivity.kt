2425095e7d9da604127778495f32f429dbee71fa
Ikrom <ikromjon.rajabov@alif.tj>
2024-08-07 14:18:04 +0500

    first commit

---
+++ app/src/main/java/tj/ikrom/guidetj/MainActivity.kt
@@ -0,0 +1,39 @@
+package tj.ikrom.guidetj
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import tj.ikrom.guidetj.ui.theme.GuideTjTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GuideTjTheme {
+                Scaffold (modifier = Modifier.fillMaxSize()) {
+                    Column (
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Text(
+                            text = "Guide Tj",
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
